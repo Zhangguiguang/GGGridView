16f9317c8d27a1547aaebd42d4db9d42e74e0b72
GG <zhanggzgzhz@gmail.com>
2021-01-23 00:04:21 +0800

    pod create
    Signed-off-by: GG <zhanggzgzhz@gmail.com>

---
+++ Example/Pods/Target Support Files/GGGridView/GGGridView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GGGridView : NSObject
+@end
+@implementation PodsDummy_GGGridView
+@end
---
+++ Example/Pods/Target Support Files/Pods-GGGridView_Example/Pods-GGGridView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GGGridView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GGGridView_Example
+@end
